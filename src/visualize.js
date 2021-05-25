c78a41422ff872751243c7cf22537d35ddfc242d
ZacharyACoon <69929375+ZacharyACoon@users.noreply.github.com>
2021-05-25 01:15:47 -0700

    semicolons?

--- src/visualize.js
+++ src/visualize.js
@@ -5,7 +5,7 @@ console.log(d3);
 export default sampleFile => {
     d3.json(sampleFile).then(data => {
         // cleanup java's millisecond timestamps
-        data.nodes = data.nodes.map(n => {n.timestamp /= 1000; return n})
+        data.nodes = data.nodes.map(n => {n.timestamp /= 1000; return n});
 
         let container = d3.select("body");
         let w = () => container.node().clientWidth;
@@ -22,21 +22,21 @@ export default sampleFile => {
             .attr("fill", "currentColor")
             .style("text-anchor", "middle")
             .style("font-size", "125%")
-            .style("text-decoration", "underline")
+            .style("text-decoration", "underline");
 
 
     // scale x
         let min_timestamp = Math.min(...data.nodes.map(d => d.timestamp));
         let max_timestamp = Math.max(...data.nodes.map(d => d.timestamp));
         let x = d3.scaleLinear().domain([min_timestamp, max_timestamp]).range([150, w() - 100]);
-        let axis_x = d3.axisBottom(x)
+        let axis_x = d3.axisBottom(x);
         let axis_x_draw = g.append("g").attr("class", "scale_x")
             .attr("transform", `translate(0, ${h() - h() * 0.05})`)
-            .call(axis_x)
+            .call(axis_x);
         axis_x_draw.append("text").text("Timestamp")
                 .attr("transform", `translate(${w() / 2}, ${h() * 0.033})`)
                 .attr("fill", "currentColor")
-                .style("text-anchor", "middle")
+                .style("text-anchor", "middle");
 
 
     // scale y
@@ -49,11 +49,11 @@ export default sampleFile => {
             .tickFormat(d3.format('d'));
         let axis_y_draw = g.append("g").attr("class", "scale_y")
             .attr("transform", `translate(${w() * 0.05}, 0)`)
-            .call(axis_y)
+            .call(axis_y);
         axis_x_draw.append("text").attr("transform", `translate(${-w() * 0.033}, ${h() / 2})`)
             .attr("fill", "currentColor")
             .style("text-anchor", "middle")
-            .text("Shard")
+            .text("Shard");
 
 
     // gridlines x
@@ -66,12 +66,12 @@ export default sampleFile => {
             .attr("y2", h() - h() * 0.05)
             .style("stroke", "white")
             .style("stroke-opacity", 0.1)
-            .style("stroke-dasharray", "1")
+            .style("stroke-dasharray", "1");
 
 
         // links
-        let n = (i) => data.nodes[i - 1]
-        let color_ref = (l) => `s${l.source}t${l.target}`
+        let n = (i) => data.nodes[i - 1];
+        let color_ref = (l) => `s${l.source}t${l.target}`;
         let links = g.append("g").attr("class", "links").selectAll("line").data(data.links).enter()
             .append("line")
                 .attr("x1", l => x(n(l.source).timestamp))
@@ -91,7 +91,7 @@ export default sampleFile => {
                     link_doms
                         .style("stroke", l => `url(#${color_ref(l)})`)
                         .style("stroke-opacity", 0.5)
-                })
+                });
 
         // gradients
         let gradients = g.select(".links").append("defs").selectAll("linearGradient").data(data.links).enter()
@@ -103,9 +103,9 @@ export default sampleFile => {
             .attr("y2", l => y(n(l.target).shard))
             .attr("gradientUnits", "userSpaceOnUse")
             .call(selection => {
-                selection.append("stop").attr("stop-color", l => color_index(n(l.source).shard)).attr("offset", 0)
-                selection.append("stop").attr("stop-color", l => color_index(n(l.target).shard)).attr("offset", 1)
-            })
+                selection.append("stop").attr("stop-color", l => color_index(n(l.source).shard)).attr("offset", 0);
+                selection.append("stop").attr("stop-color", l => color_index(n(l.target).shard)).attr("offset", 1);
+            });
 
     // block info
         let info = container.append("div").attr("class", "info")
@@ -113,7 +113,7 @@ export default sampleFile => {
             .style("padding", "1em")
             .style("color", "white")
             .style("text-anchor", "middle")
-            .text("<click_node_to_get_data>")
+            .text("<click_node_to_get_data>");
 
 
     // nodes
@@ -128,7 +128,7 @@ export default sampleFile => {
                 .on("mouseover", (event, node) => {
                     d3.select(event.target)
                         .style("fill", "white")
-                        .style("stroke", "black")
+                        .style("stroke", "black");
 
                     links
                         .style("stroke", l => (l.source === node.id) ? "white" : `url(#${color_ref(l)})`)
@@ -140,7 +140,7 @@ export default sampleFile => {
                 .on("mouseout", (event, d) => {
                     d3.select(event.target)
                         .style("fill", a => color_index(a.shard))
-                        .style("stroke", "white")
+                        .style("stroke", "white");
                     links
                         .style("stroke", l => `url(#${color_ref(l)})`)
                         .style("stroke-opacity", 0.5)
@@ -153,36 +153,34 @@ export default sampleFile => {
             svg.attr("viewBox", `0 0 ${w()} ${h()}`);
             svg.call(d3.zoom().extent([[0, 0], [w(), h()]]).scaleExtent([0.25, 100])
                 .on("zoom", ({transform}) => {
-                    nx = transform.rescaleX(x)
-                    ny = transform.rescaleY(y)
-                    let n = (i) => data.nodes[i - 1]
+                    let nx = transform.rescaleX(x);
+                    let ny = transform.rescaleY(y);
+                    let n = (i) => data.nodes[i - 1];
                     links
                         .attr("x1", d => nx(n(d.source).timestamp))
                         .attr("y1", d => ny(n(d.source).shard))
                         .attr("x2", d => nx(n(d.target).timestamp))
-                        .attr("y2", d => ny(n(d.target).shard))
+                        .attr("y2", d => ny(n(d.target).shard));
                     gradients
                         .attr("x1", d => nx(n(d.source).timestamp))
                         .attr("y1", d => ny(n(d.source).shard))
                         .attr("x2", d => nx(n(d.target).timestamp))
-                        .attr("y2", d => ny(n(d.target).shard))
+                        .attr("y2", d => ny(n(d.target).shard));
                     nodes
                         .attr("cx", d => nx(d.timestamp))
-                        .attr("cy", d => ny(d.shard))
+                        .attr("cy", d => ny(d.shard));
                     gridlines_x
                         .attr("x1", n => nx(n.timestamp))
                         .attr("y1", n => ny(n.shard))
                         .attr("x2", n => nx(n.timestamp))
-                        .attr("y2", h() - h() * 0.05)
+                        .attr("y2", h() - h() * 0.05);
 
-                    axis_x.scale(nx)
+                    axis_x.scale(nx);
                     axis_x_draw
                         .call(axis_x)
-                        .attr("transform", `translate(0, ${h() - h() * 0.05})`)
-                    axis_y.scale(ny)
-                    axis_y_draw.call(axis_y)
-
-                    // g.attr("transform", transform)
+                        .attr("transform", `translate(0, ${h() - h() * 0.05})`);
+                    axis_y.scale(ny);
+                    axis_y_draw.call(axis_y);
                 }));
         }
 
